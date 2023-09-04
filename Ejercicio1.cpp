bef5b08c075a98dc42d117f50f92870237188c68
lenon65 <lenonpreto65@gmail.com>
2023-09-04 09:58:21 -0300

    Terminado y Comentado

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -2,12 +2,15 @@
 
 
 int main() {
-    Lugar YPF(5.0);
-    Lugar SHELL(10.0);
-    Lugar Atalaya(20.0);
+    Lugar YPF(5.0);             // instacia un objeto Lugar con el nombre de YPF al KM 5
+    Lugar SHELL(10.0);          // instacia un objeto Lugar con el nombre de SHELL al KM 10
+    Lugar Atalaya(20.0);        // instacia un objeto Lugar con el nombre de Atalaya al KM 20
     
-    Pepita pepita(100.0,&YPF);
+    Pepita pepita(100.0,&YPF);  // instacia un objeto Pepita con el nombre de pepita con 100 J y ubicacion inicial en YPF
     
+    /*
+    Los siguientes llamados son para comprobar los distintos escenarios. 
+    */
     pepita.comer(2.0);
     cout << "La energía de pepita es " << pepita.MostrarEnergia() << " y se encuentra en el km " << pepita.MostrarUbicacion() <<endl;
     pepita.volar(&SHELL);
--- ejercicio1.h
+++ ejercicio1.h
@@ -5,21 +5,13 @@
 
 using namespace std;
 
-class Lugar {
+class Pepita { // Declaración de la clase "Pepita"
 public:
-    Lugar(float const v) : km(v) {}
-
-    float NroKm() {
-        return km;
-    }
-private:
-    float km;
-};
-
-class Pepita {
-public:
-    Pepita(float e, Lugar *inicio) : energia(e), ubicacion(inicio) {}
-    
+    Pepita(float e, Lugar *inicio) : energia(e), ubicacion(inicio) {} // Declaración del constructor
+    /*  
+    Función volar: recibe el destino, saca la distancia (en valor absoluto) y se fija si puede llegar o no.
+    Si llega consume la enegia necesaria y se desplaza al lugar, sino avisa que no puede por falta de energia.
+    */
     void volar(Lugar *destino) {
         float distancia = abs(destino->NroKm() - ubicacion->NroKm());
         float consumo = 10.0 + distancia;
@@ -32,18 +24,28 @@ public:
         }
     }
     
+    /*
+    Función comer: carga energia (4J*1gr)
+    */
     void comer(float comida) {
         energia += comida * 4.0;
     }
-    
+    /*
+    Fución que devuelve la energia (float).
+    */
     float MostrarEnergia() {
         return energia;
     }
     
+    /*
+    Función que devuelve la ubicación (float).
+    */
     float MostrarUbicacion() {
         return ubicacion->NroKm();
     }
-    
+    /*
+    Función que muestra por pantalla si pepita puede llegar al lugar requerido o no.
+    */
     void Consulta (float p){
         float dist;
         dist=abs(p - ubicacion->NroKm());
@@ -53,9 +55,25 @@ public:
             cout<<"Pepita puede NO ir al Km "<<p<<" me falta "<<abs(energia-10.0-dist)<<" joules o "<<abs(energia-10.0-dist)/4.0 <<" gr de comida"<<endl;
     }
 
-private:
+private:    // Atributos
     float energia;
     Lugar * ubicacion;
+};
+
+/*
+Declaración de la clase Lugar
+*/
+class Lugar {
+public:
+    Lugar(float const v) : km(v) {} // Declaración del constructor
+    /*
+    Función que devuelve el km (float).
+    */
+    float NroKm() {
+        return km;
+    }
+private:    // Atributos
+    float km;
 };
 
 #endif
